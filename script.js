c65d0005ee9622a4b2271c37d29ddf5920c047b2
LΞO <matysek.leos@gmail.com>
2024-02-13 21:11:26 +0100

    update script.js
    canva - width, height

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-pepa
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('enviroment');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1000;
+    canvas.height = 600;
+    
+});
