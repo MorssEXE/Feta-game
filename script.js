35570b48cd3b7853a7add2304ba5f23d39568a0b
Daniel Tomis <danko.tomis@seznam.cz>
2024-02-22 17:22:25 +0100

    add script.js
    script pro runovani cele hry

--- script.js
+++ script.js
@@ -1,7 +1,67 @@
-window.addEventListener('load', function(){
-    const canvas = document.getElementById('enviroment');
-    const ctx = canvas.getContext('2d');
-    canvas.width = 1000;
-    canvas.height = 600;
-    
-});
+import { setupGround, updateGround } from "./ground.js"
+import { updateMis, setupMis } from "./mis.js"
+
+const WORLD_WIDTH = 100
+const WORLD_HEIGHT = 30
+const SPEED_SCALE_INCREASE = 0.00001
+
+const worldElement = document.querySelector('[data-world]')
+const scoreElement = document.querySelector('[data-score]')
+const startScreenElement = document.querySelector('[data-start-screen]')
+
+setPixelToWorldScale()
+window.addEventListener("resize", setPixelToWorldScale)
+document.addEventListener("keydown", handleStart, {once: true})
+
+let lastTime
+let speedScale
+let score
+
+function update(time) {
+    if (lastTime == null) {
+        lastTime = time;
+        window.requestAnimationFrame(update)
+        return
+    }
+    const delta = time - lastTime
+
+    updateGround(delta, speedScale)
+    updateMis(delta, speedScale)
+    updateSpeedScale(delta)
+    updateScore(delta)
+
+    lastTime = time;
+    window.requestAnimationFrame(update)
+}
+
+function updateSpeedScale(delta) {
+    speedScale += delta * SPEED_SCALE_INCREASE
+}
+
+function updateScore(delta) {
+    score += delta * 0.01
+    scoreElement.textContent = Math.floor(score)
+}
+
+function handleStart() {
+    lastTime = null
+    speedScale = 1
+    score = 0
+    setupGround()
+    setupMis()
+    startScreenElement.classList.add("hide")
+    window.requestAnimationFrame(update)
+}
+
+function setPixelToWorldScale() {
+    let worldToPixelScale
+    if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) 
+    {
+        worldToPixelScale = window.innerWidth / WORLD_WIDTH
+    } else {
+        worldToPixelScale = window.innerHeight / WORLD_HEIGHT
+    }
+
+    worldElement.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
+    worldElement.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
+}
