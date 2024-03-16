e1dd81a18e53262db0ce7ce8c0c29a1d11233dd4
Leoš Matýsek <159525377+FetaEatingMouse@users.noreply.github.com>
2024-03-16 21:40:47 +0100

    add styles

--- leaderboard.php
+++ leaderboard.php
@@ -4,12 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Feta | Leaderboard</title>
+    <style type ="text/css">
+        table{
+            border-collapse: collapse;
+            width: 100%;
+            font-size: 25px;
+            text-align: left;
+        }    
+    </style>
 </head>
 <body>
     <table>
         <h1>LEADERBOARD</h1>
         <tr>
-            <th></th>
+            <th>Position</th>
             <th>Username</th>
             <th>Score</th>
         </tr>
@@ -19,19 +27,12 @@
             $sql = "SELECT nickname, score from users";
             $result = $conn-> query($sql);
 
-            if ($result-> num_rows > 0) {
                 while ($row = $result-> fetch_assoc()) {
-                    echo "<tr><td>". $row["nickname"] ."</td><td>". $row["score"] ."</td></tr>"; 
+                    echo "<tr><td>"."position" ."</td><td>". $row["nickname"] ."</td><td>". $row["score"] ."</td></tr>"; 
                 }
-                echo "</table>";
-            }
-            else {
-                echo "0 result";
-            }
-
             $conn-> close();
         ?>
-      <a href="index.php"><button>back</button></a>
     </table>
+    <a href="index.php"><button>back</button></a>
 </body>
 </html>
